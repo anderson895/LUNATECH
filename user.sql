ea328d6eedb3828a15ec2ad4c99a6f14255f84b1
JARP <andersonandy046@gmail.com>
2025-03-17 09:35:07 +0800

    update

---
+++ user.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 17, 2025 at 02:34 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lunatech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `user_fullname` varchar(60) NOT NULL,
+  `user_username` varchar(60) NOT NULL,
+  `user_email` varchar(60) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_type` varchar(60) NOT NULL,
+  `user_status` int(11) NOT NULL DEFAULT 1 COMMENT '0=deleted,1=existing',
+  `Branch_code` varchar(60) DEFAULT NULL,
+  `Branch_name` varchar(60) DEFAULT NULL,
+  `Branch_address` text DEFAULT NULL,
+  `Branch_started` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `user_fullname`, `user_username`, `user_email`, `user_password`, `user_type`, `user_status`, `Branch_code`, `Branch_name`, `Branch_address`, `Branch_started`) VALUES
+(1, 'rick sanchez', 'admin', 'admin@gmail.com', '$2y$10$wFakoThTmlWnmqKwwF25heSn4jGSEuhOTtODtmU8sKAiUpnQ0CvjS', 'Administrator', 1, NULL, '', '', NULL),
+(2, 'joshua padilla', 'andersonandy046', 'andersonandy046@gmail.com', '$2y$10$qYtfNuDcs13sLrcAQ.ic0uOZ70TU03BuU64bTHUhG.clQz9gt5ypG', 'General Manager', 1, NULL, '', '', NULL),
+(3, 'joshua padilla', 'andersonandy046', 'andersonandy046@gmail.com', '$2y$10$sG/04wrOKoY/0f50ocgSJuZSRZCw/ccmvhfc1Exa.vos5WGa4dZgm', 'General Manager', 1, NULL, '', '', NULL),
+(4, 'test test', 'test', 'test@gmail.com', '$2y$10$TNMDFrM4cGrdr.dIzBLuB.C1UHlWFSMZMsS4wZ3qDlISR2oTlTaFW', 'General Manager', 1, NULL, '', '', NULL),
+(5, 'diwata', 'diwata', 'diwata@gmail.com', '$2y$10$e4kRCgNshIafNtj6DzCVbe/tv3oByiUzHvP/Y/FZ4IiE1JrxcqdiG', 'General Manager', 1, NULL, '', '', NULL),
+(6, 'ako', 'ako', 'ako2@gmail.com', '$2y$10$ZdzM8cgUxAjg8AE//2MngOc2Q1RKWYQjz.mMMxwAdUG4oZnG4x2jy', 'General Manager', 1, NULL, '', '', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
